316dbdda40ec9e9cdd9ffff7411c4350e0f2481f
kajj8808 <a01039388808@gmail.com>
2024-09-01 22:06:05 +0900

    ✅ admin page 수정 ( subtitle upload 작성 )

--- src/routes/subtitle.ts
+++ src/routes/subtitle.ts
@@ -1,10 +1,89 @@
-import { Router } from "express";
+import e, { Router } from "express";
 import multer from "multer";
-import { SUBTITLE_FOLDER_DIR } from "../lib/constants";
+import fs from "fs";
+import { SUBTITLE_FOLDER_DIR, VIDEO_FOLDER_DIR } from "../lib/constants";
+import path from "path";
+import { addAssSubtitleToVideo } from "../lib/ffmpeg";
+import db from "../lib/db";
+
+const fileFilter = (
+  req: e.Request,
+  file: Express.Multer.File,
+  cb: multer.FileFilterCallback
+) => {
+  const typeArray = file.mimetype.split("/");
+  const fileType = typeArray[1];
+
+  if (fileType == "smi" || fileType == "ass") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
 const subtitleRouter = Router();
-const subtitleUpload = multer({ dest: `${SUBTITLE_FOLDER_DIR}` });
+const subtitleUpload = multer({
+  dest: `${SUBTITLE_FOLDER_DIR}`,
+  fileFilter: fileFilter,
+});
 
-subtitleRouter.post(":id", subtitleUpload.single("subtitle"), (req, res) => {});
+const checkAssFile = (filename: string) => filename.includes(".ass");
+
+subtitleRouter.post(
+  "/",
+  subtitleUpload.single("subtitle"),
+  async (req, res) => {
+    const file = req.file;
+    const episodeId = req.body.episode_id as number;
+    if (file && episodeId) {
+      const subtitleId = new Date().getTime();
+      fs.renameSync(
+        file.path,
+        path.join(`${SUBTITLE_FOLDER_DIR}/${subtitleId}`)
+      );
+      await db.episode.update({
+        where: {
+          id: episodeId,
+        },
+        data: {
+          subtitle_id: subtitleId.toString(),
+          is_ass: checkAssFile(file.originalname),
+        },
+      });
+      res.json({
+        ok: true,
+        subtitleId: subtitleId,
+      });
+      if (req.body.is_overlap) {
+        try {
+          await addAssSubtitleToVideo({
+            assPath: `${SUBTITLE_FOLDER_DIR}/${subtitleId}`,
+            videoOutPath: `${VIDEO_FOLDER_DIR}/${req.body.video_id}.mp4`,
+            videoPath: `${VIDEO_FOLDER_DIR}/${req.body.video_id}`,
+          });
+          fs.rmSync(`${VIDEO_FOLDER_DIR}/${req.body.video_id}`);
+          fs.renameSync(
+            `${VIDEO_FOLDER_DIR}/${req.body.video_id}.mp4`,
+            `${VIDEO_FOLDER_DIR}/${req.body.video_id}`
+          );
+          await db.episode.update({
+            where: {
+              id: episodeId,
+            },
+            data: {
+              is_overlap: true,
+            },
+          });
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    } else {
+      return res.json({
+        ok: false,
+      });
+    }
+  }
+);
 
 export default subtitleRouter;
