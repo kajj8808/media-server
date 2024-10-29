24cdebef746c8c7872cd5dc529482b8efe0129f8
kajj8808 <a01039388808@gmail.com>
2024-10-29 21:32:33 +0900

    ✅ 코드 정리 + video 정리 코드 정리

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,12 @@ import {
   animationKorDescriptionAutoUpdate,
 } from "./lib/auto";
 import { createServer } from "./lib/http";
-import { PORT, SUBTITLE_FOLDER_DIR, VIDEO_FOLDER_DIR } from "./lib/constants";
-import { readSubtitleFileData, sleep } from "./lib/utils";
+import { PORT } from "./lib/constants";
+import { sleep } from "./lib/utils";
 
 import seriesRouter from "./routes/series";
 import videoRouter from "./routes/video";
 import subtitleRouter from "./routes/subtitle";
-import path from "path";
-import bodyParser from "body-parser";
 import cors from "cors";
 
 const app = express();
@@ -33,39 +31,9 @@ server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-// TEST
-/* const videoPath = path.join(VIDEO_FOLDER_DIR, "T.mkv");
-const videoOutPath = path.join(VIDEO_FOLDER_DIR, "T.mp4");
-const assPath = path.join(SUBTITLE_FOLDER_DIR, "T.ass");
-
-addAssSubtitleToVideo({ videoPath, assPath, videoOutPath }); */
-
-/* import fs from "fs";
-
-import { convertAssToVtt } from "./lib/subtitle/assToVtt";
-import { convertSmiToVtt } from "./lib/subtitle/smiToVtt";
-(async () => {
-  const assPath = path.join(SUBTITLE_FOLDER_DIR, "T.ass");
-  const smiPath = path.join(SUBTITLE_FOLDER_DIR, "w.smi");
-
-  const filedata = await readSubtitleFileData(assPath);
-  //const filedata = await readSubtitleFileData(smiPath);
-  console.log(filedata);
-  console.log(convertSmiToVtt(filedata));
-})();
- */
-/* while (true) {
-  // 1 day wait
-  await sleep(1 * 60 * 60 * 24);
-}
- */
-
-
-
 while (true) {
   animationAutoDownload();
   animationKorDescriptionAutoUpdate();
   // 4 hour wait
   await sleep(1 * 60 * 60 * 4);
 }
-
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -22,10 +22,37 @@ export async function getProminentColorHexCode(imageUrl: string) {
 import fs from "fs";
 import iconv from "iconv-lite";
 import chardet from "chardet";
+import db from "./db";
+import { VIDEO_FOLDER_DIR } from "./constants";
+import path from "path";
 export async function readSubtitleFileData(filePath: string) {
   const fileData = fs.readFileSync(filePath);
   const encoding = chardet.detect(fileData);
   const decodeData = iconv.decode(fileData, encoding || "utf-8");
 
   return decodeData;
+}
+export async function clenupVideos() {
+  const episodes = await db.episode.findMany({
+    select: {
+      video_id: true,
+    },
+  });
+
+  const videoIds = fs.readdirSync(VIDEO_FOLDER_DIR);
+  const notExists = videoIds;
+
+  for (let episode of episodes) {
+    for (let videoId of videoIds) {
+      if (videoId === episode.video_id) {
+        notExists.pop();
+        break;
+      }
+    }
+  }
+
+  notExists.forEach((videoId) => {
+    const videoPath = path.join(VIDEO_FOLDER_DIR, videoId);
+    console.log(videoPath);
+  });
 }
