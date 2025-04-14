94d6425b91908e8b8b29f7b0e273f02d21ad0bcd
kajj8808 <a01039388808@gmail.com>
2025-04-15 08:54:23 +0900

    📦 image의 평균 색상을 가져오기 위해 node-vibrant 설치 + 이전에 사용하던 jimp 삭제

--- src/services/discord.ts
+++ src/services/discord.ts
@@ -4,7 +4,7 @@ const webhookClient = new WebhookClient({
   url: "https://discord.com/api/webhooks/869305319384293456/iCxdyMvKJl4-7kKf6Kybjd63JC1O3ENuQCnhTluWCShf2OlpAWwbSsT680LObMD6huV-",
 });
 
-const embed = new EmbedBuilder()
+/* const embed = new EmbedBuilder()
   .setDescription(
     `-# ott \n### [series_name season_number episode_number episode_name](https://www.youtube.com/watch?v=lTNsFt31u30)\n`
   )
@@ -17,3 +17,11 @@ const embed = new EmbedBuilder()
 webhookClient.send({
   embeds: [embed],
 });
+ */
+
+import { Vibrant } from "node-vibrant/node";
+
+const imageUrl =
+  "https://image.tmdb.org/t/p/original/aYoKoKN0EYO9pNm0GNHqHtcZfqw.jpg";
+
+(async () => {})();
