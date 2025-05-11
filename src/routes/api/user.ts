6ae8f95f49c06fb44b507c2f2b12c3b44cb8d65b
kajj8808 <a01039388808@gmail.com>
2025-05-12 03:50:38 +0900

    ✅ user 의 watch record 를 등록 하는 부분 자것ㅇ

--- src/routes/api/user.ts
+++ src/routes/api/user.ts
@@ -33,9 +33,53 @@ userRouter.post("/log-in", async (req, res) => {
 });
 
 userRouter.post("/watch-record", async (req, res) => {
-  const { watchId, duration, currentTime } = req.body;
-  console.log(watchId, duration, currentTime);
+  const { watchId, duration, currentTime, userId } = req.body;
 
+  const videoContet = await db.videoContent.findFirst({
+    where: {
+      watch_id: watchId,
+    },
+    include: {
+      movie: true,
+      episode: true,
+    },
+  });
+  if (!videoContet) {
+    res.json({
+      ok: false,
+      message: "Bad Request...",
+    });
+    return;
+  }
+  // 95퍼센트 이상 보았다면 completed상태로 저장.
+  const watchStatus =
+    (currentTime / duration) * 100 > 95 ? "COMPLETED" : "WATCHING";
+  const episodeId = videoContet.episode?.id;
+  const movieId = videoContet.movie?.id;
+  const seasonId = videoContet.season_id;
+  const seriesId = videoContet.series_id;
+  await db.userWatchProgress.upsert({
+    create: {
+      video_content_id: videoContet.id,
+      user_id: userId,
+      current_time: currentTime,
+      total_duration: duration,
+      status: watchStatus,
+      episode_id: episodeId,
+      movie_id: movieId,
+      season_id: seasonId,
+      series_id: seriesId,
+    },
+    update: {
+      current_time: currentTime,
+    },
+    where: {
+      user_id_video_content_id: {
+        user_id: userId,
+        video_content_id: videoContet.id,
+      },
+    },
+  });
   res.send("ok");
 });
 
