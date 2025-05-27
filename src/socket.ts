12b062b2d66789b247fcd399672530050cc5a94e
kajj8808 <a01039388808@gmail.com>
2025-05-27 20:10:52 +0900

    ✅ video sync부분에 필요한 코드 추가

--- src/socket.ts
+++ src/socket.ts
@@ -1,7 +1,11 @@
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 
 import { type Server as HttpServer } from "http";
 import { type Server as HttpsServer } from "https";
+
+interface CustomSocket extends Socket {
+  roomName?: string;
+}
 
 let socketServer: Server;
 
@@ -12,9 +16,25 @@ export function initSocket(server: HttpsServer | HttpServer) {
       methods: ["GET", "POST"],
     },
   });
-  socketServer.on("connection", (socket) => {
+  socketServer.on("connection", (socket: CustomSocket) => {
     socket.on("joinRoom", (roomName) => {
-      console.log(roomName);
+      socket.join(roomName);
+      socket.roomName = roomName;
+    });
+    socket.on("timeUpdate", (time) => {
+      if (socket.roomName) {
+        socket.to(socket.roomName).emit("timeUpdate", time);
+      }
+    });
+    socket.on("pause", () => {
+      if (socket.roomName) {
+        socket.to(socket.roomName).emit("pause");
+      }
+    });
+    socket.on("play", () => {
+      if (socket.roomName) {
+        socket.to(socket.roomName).emit("play");
+      }
     });
   });
 }
